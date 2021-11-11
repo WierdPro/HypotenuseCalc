b93cb1e2f556f2f32b1494c2623f8cc7583506ca
WierdPro <94013950+WierdPro@users.noreply.github.com>
2021-11-11 13:24:36 +0000

    Add files via upload

--- src/md/wierdpro/hypotenuse/Main.java
+++ src/md/wierdpro/hypotenuse/Main.java
@@ -1,17 +1,43 @@
 package md.wierdpro.hypotenuse;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-                System.out.println("Lesson 7 GUI");
-                System.out.println("Hypotenuse GUI\n");
 
-                double x = Double.parseDouble(JOptionPane.showInputDialog("Enter side x"));
-                double y = Double.parseDouble(JOptionPane.showInputDialog("Enter side y"));
-                double z = Math.sqrt((x*x) + (y*y));
+        //Importing operators
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
 
-                JOptionPane.showMessageDialog(null, "x = " + x + "\ny = " + y + "\nThe hypotenuse is: " + z);
+        //Adding text, text area and buttons
+        JLabel sideXText = new JLabel("Enter side x");
+        TextArea x = new TextArea();
+
+        //Fonts and size
+        sideXText.setFont(new Font("Ink Free", 2, 28));
+
+
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        panel.add(sideXText);
+        panel.add(x);
+        frame.add(panel);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setTitle("Hypotenuse calculator v1.1.0-beta");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //frame.pack();
+
+        frame.setVisible(true);
+
+
+//                double x = Double.parseDouble(JOptionPane.showInputDialog("Enter side x"));
+//                double y = Double.parseDouble(JOptionPane.showInputDialog("Enter side y"));
+//                double z = Math.sqrt((x*x) + (y*y));
+
+                //JOptionPane.showMessageDialog(null, "x = " + x + "\ny = " + y + "\nThe hypotenuse is: " + z);
 
     }
 }
